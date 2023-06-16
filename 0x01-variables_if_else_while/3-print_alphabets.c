897696c9162bc38a133306a620dc7cf0346abb79
mmgmolotov <mmgmolotov@gmail.com>
2023-06-16 12:48:02 -0700

    alphabetALPHABET print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,14 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++):n
-
-		printf("%c", alphabet);
-
-	printf("\n");
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar(alphabet);
+}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+{
+	putchar(alphabet);
+}
+	putchar('\n');
 	return (0);
 }
