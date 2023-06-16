69c26b31b950d2cc31ad939be5b4f254edf3d2c2
mmgmolotov <mmgmolotov@gmail.com>
2023-06-16 14:45:11 -0700

    combi single num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	int cnum;
+
+	for (cnum = 0 ; cnum <= 9 ; cnum++)
+	{
+		putchar(cnum +'0');
+		if (cnum == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
