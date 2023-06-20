170ebf975ad00d09beb9b36863472702ba004114
mmgmolotov <mmgmolotov@gmail.com>
2023-06-20 15:52:29 -0700

    adds 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - function that adds two integers
+*@a: 1st number to compute
+*@b: 2nd number to compute
+*
+*Return: result
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
