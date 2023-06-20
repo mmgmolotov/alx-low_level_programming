da2c2ea461434a9fdda3d3f30515933eb07819c0
mmgmolotov <mmgmolotov@gmail.com>
2023-06-20 05:00:45 -0700

    _islowerascii

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_islower - check for low_case char
+ *@c: is the char need to be checked
+ *
+ *Return: 1 for lowercase or 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
