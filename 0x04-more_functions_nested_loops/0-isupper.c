d279e013fbc3ab77a35bcb0641637003811bdbf6
mmgmolotov <mmgmolotov@gmail.com>
2023-06-21 23:13:44 -0700

    isupper_if

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_isupper - check for sup_case char
+*@c: is the char need to be checked
+*
+*Return: 1 for uppercase or 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+	_putchar('\n')
+}
