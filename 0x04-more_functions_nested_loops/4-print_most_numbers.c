56d6b3a1acd42b852aa09e8ca3649b8576fb4c8f
mmgmolotov <mmgmolotov@gmail.com>
2023-06-22 11:15:35 -0700

    except2/4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - function print numbers
+ * 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0' ;  n <= '9' ; n++)
+	{
+		if ((n != '2' ) && (n != '4'))
+		_putchar(n);
+	}
+	_putchar('\n');
+}
