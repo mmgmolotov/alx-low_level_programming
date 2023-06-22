715b9f943541fdb7e0269086436deec8cc964c38
mmgmolotov <mmgmolotov@gmail.com>
2023-06-22 04:53:40 -0700

    print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Function that print from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
