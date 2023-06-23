c4977a213e2dda847b68af87eac7d529a2d9b922
mmgmolotov <mmgmolotov@gmail.com>
2023-06-22 17:08:53 -0700

    squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,22 +8,18 @@
   */
 void print_square(int size)
 {
-	int a, b;
+	int i, j;
 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+	_putchar('\n');
+	}
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
-	{
-		for (a = 0; a < size; a++)
-		{
-			for (b = 0; b < size; b++)
-			{
-				_putchar(b);
-			}
-
-			_putchar('\n');
-		}
 	}
 }
