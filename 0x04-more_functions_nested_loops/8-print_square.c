cd6444a2915a95228a031897bd67c921b61178a0
mmgmolotov <mmgmolotov@gmail.com>
2023-06-22 17:02:57 -0700

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * print_square - Prints n squares / n number of times
+  * @size: nbr of squares/number of times
+  *
+  * Return: void
+  */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(b5);
+			}
+
+			_putchar('\n');
+		}
+	}
+}
