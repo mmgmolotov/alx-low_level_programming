e06115b1c5ddd992918a53581f58c9e49e30c573
mmgmolotov <mmgmolotov@gmail.com>
2023-07-05 11:43:38 -0700

    3.task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,7 @@
  *factorial - function that returns the factorial of a given number.
  *@n:given number
  *
- *Return - If n is lower than 0 return  -1 for errors
+ *Return - If n is lower than 0 return  -1 for errors,retur factorial
  */
 int factorial(int n)
 {
