9e0471c122cc66a3ce6790b2703786be3a105294
mmgmolotov <mmgmolotov@gmail.com>
2023-07-05 09:41:21 -0700

    0.task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,20 @@
 /**
  *_puts_recursion - function that prints a string,
  *followed by a new line.
+ *@s:string need to be printed.
  *
  *Return: void
  */
 void _puts_recursion(char *s)
 {
 	_putchar(*s);
-	s++;
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
 	if (*s)
 	{
+		s++;
 		_puts_recursion(s);
-	}
-	if (*s == ('\0'))
-	{
-		_putchar('\n');
 	}
 }
