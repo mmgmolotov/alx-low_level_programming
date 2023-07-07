0e6295156125321026ebec86e195c1ea95da897d
mmgmolotov <mmgmolotov@gmail.com>
2023-07-07 13:32:11 -0700

    2 task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print num of arguments
+ *@argc:count argument
+ *@argv:vector argument
+ *
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc ; i++)
+	{
+		printf("%d - %s\n", i, argv[i]);
+	}
+	return (0);
+}
