c5ea73c8d6af4c92c21e9847e2647fcff6a0f84b
mmgmolotov <mmgmolotov@gmail.com>
2023-07-07 12:26:40 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print num of arguments
+ * @argc: count argument
+ * @argv: vector argument
+ *
+ * Return: always 0
+ */ 
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		return (0);
+	}
+}
+
