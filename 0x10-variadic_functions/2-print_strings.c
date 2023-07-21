db2a5d30ed8080d5c1b4d080eb2ecf157c03779c
mmgmolotov <mmgmolotov@gmail.com>
2023-07-20 17:27:01 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,14 +14,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 	va_list args;
-
-	str = 0;
+	
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		if (str != NULL)
+		str = va_arg(args, char*);
+		if (str == NULL)
 			printf("(nil)");
-		printf("%s", va_arg(args, char *));
+		printf("%s", str);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
