34625459de2fb9ddb04549871379939a36fcb59e
mmgmolotov <mmgmolotov@gmail.com>
2023-07-27 11:51:49 -0700

    task 0 & task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,11 +16,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str != NULL)
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		else
 		{
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n"; 0, (nil));
 		}
 		count++;
 		h = h->next;
