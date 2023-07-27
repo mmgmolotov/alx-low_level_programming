19c6202d50d54dda080b634d09a60aadce525cb3
mmgmolotov <mmgmolotov@gmail.com>
2023-07-27 16:59:45 -0700

    task 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +23,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
 
 #endif /* LISTS_H */
