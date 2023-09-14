67649b149c711680493898593df07506fc3c7534
mmgmolotov <mmgmolotov@gmail.com>
2023-09-14 13:57:28 -0700

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,27 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * add_dnodeint - func add node
+ * @head: arg
+ * @n: arg
+ * Return: new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	
+	dlistint_t *new;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
+	return (new);
 }
