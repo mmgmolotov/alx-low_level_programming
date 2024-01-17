f85f6937f24cb68c486e18b15d035f774031546e
mmgmolotov <mmgmolotov@gmail.com>
2024-01-17 23:34:51 +0100

    Remove unnecessary comment in linear_search function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,7 +3,6 @@
 /**
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
- *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
