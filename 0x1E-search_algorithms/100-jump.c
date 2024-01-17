52917018df84f50ab33a541ea72eca91890b7532
mmgmolotov <mmgmolotov@gmail.com>
2024-01-17 23:46:41 +0100

    Refactor variable names for clarity and consistency

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
